f6da81317f4965b4dedc93b2378cb4b68b602744
juan <21977733+juanmed@users.noreply.github.com>
2020-01-19 18:06:53 +0900

    - Separated controller into high and mid level controllers. The former computes thrust, thrust vector and desired orientation and also works at a slower frequency, and the latter computes angular velocity and should work at a higher frequency.

--- riseq_control/include/riseq_control/position_controller.h
+++ riseq_control/include/riseq_control/position_controller.h
@@ -3,6 +3,7 @@
 
 #include <ros/ros.h>
 #include <Eigen/Dense>
+#include <iostream>
 
 #include <nav_msgs/Odometry.h>
 #include <geometry_msgs/PoseStamped.h>
@@ -23,15 +24,17 @@ class PositionController
 
 		ros::NodeHandle nh_;
 		ros::NodeHandle nh_private_;
-
 		ros::Subscriber reference_sub_;
 		ros::Subscriber state_sub_;
 		ros::Subscriber position_sub_;
 		ros::Subscriber velocity_sub_;
-
 		ros::Publisher input_publisher_;
-
+		ros::Publisher rdes_publisher_;
 		ros::Timer control_loop_timer_;
+		ros::Timer mid_control_loop_timer_;
+
+		// flow control 
+		bool enable_output_;
 
 		// reference trajectory
 		Eigen::Vector3d p_ref_;
@@ -41,6 +44,7 @@ class PositionController
 		Eigen::Vector3d s_ref_;
 		Eigen::Quaterniond q_ref_;
 		Eigen::Vector3d angular_velocity_ref_;
+		Eigen::Vector3d euler_dot_ref_;
 		double yaw_ref_;
 		double yaw_dot_ref_;
 		double yaw_ddot_ref_;
@@ -53,19 +57,30 @@ class PositionController
 
 		// control quantities
 		Eigen::Vector3d thrust_vector_;
-		Eigen::Vector3d desired_orientation_;
+		Eigen::Matrix3d desired_orientation_;
 		Eigen::Vector3d desired_angular_velocity_;
+
+		// control gains
+		Eigen::Vector3d Kp_;
+		Eigen::Vector3d Kd_;
+		Eigen::Vector3d Ki_;
+		double Kr_;
+
 
 		// controllers
 	  FeedbackLinearizationController * fb_controller_;
 
 		void computeControlInputs(const ros::TimerEvent& event);
+		void computeMidControlInputs(const ros::TimerEvent& event);
+
 		void publishControlInputs(void);
 
 		void odometryCallback(const nav_msgs::Odometry& msg);
 		void positionCallback(const geometry_msgs::PoseStamped& msg);
 		void velocityCallback(const geometry_msgs::TwistStamped& msg);
 		void trajectoryCallback(const riseq_msgs::FlatTrajectory& msg);
+
+		void initializeParameters(void);
 
 };
 
--- riseq_control/src/position_controller.cpp
+++ riseq_control/src/position_controller.cpp
@@ -1,20 +1,18 @@
 #include "riseq_control/position_controller.h"
 
 
-using namespace std;
-
 PositionController::PositionController(const ros::NodeHandle& nh, const ros::NodeHandle& nh_private): 
 	nh_(nh),
-	nh_private_(nh_private){
-
+	nh_private_(nh_private)
+	{
 		reference_sub_ = nh_.subscribe("/riseq/uav/trajectory", 1, &PositionController::trajectoryCallback, this, ros::TransportHints().tcpNoDelay());
 		state_sub_ = nh_.subscribe("/riseq/uav/state", 1, &PositionController::odometryCallback, this, ros::TransportHints().tcpNoDelay());
 		position_sub_ = nh_.subscribe("/riseq/uav/position", 1, &PositionController::positionCallback, this, ros::TransportHints().tcpNoDelay());
 		velocity_sub_ = nh_.subscribe("/riseq/uav/velocity", 1, &PositionController::velocityCallback, this, ros::TransportHints().tcpNoDelay());
-
 		input_publisher_ = nh_.advertise<mav_msgs::RateThrust>("/riseq/uav/rateThrust", 1);
-
-		control_loop_timer_ = nh_.createTimer(ros::Duration(0.01), &PositionController::computeControlInputs, this); // Define timer for constant loop rate
+		rdes_publisher_ = nh_.advertise<geometry_msgs::PoseStamped>("/riseq/uav/desired_orientation", 1);
+		control_loop_timer_ = nh_.createTimer(ros::Duration(0.02), &PositionController::computeControlInputs, this); // Define timer for constant loop rate
+		mid_control_loop_timer_ = nh_.createTimer(ros::Duration(0.01), &PositionController::computeMidControlInputs, this); // Define timer for constant loop rate
 
 		// initialize members
 		p_ref_ << 0., 0., 0.;
@@ -33,50 +31,89 @@ PositionController::PositionController(const ros::NodeHandle& nh, const ros::Nod
 		angular_velocity_ << 0., 0., 0.;
 
 		thrust_vector_ << 0., 0., 0.;
-		desired_orientation_= Eigen::Vector3d::Zero();
+		desired_orientation_= Eigen::Matrix3d::Zero();
 		desired_angular_velocity_ = Eigen::Vector3d::Zero();
 
-		double Kp_x, Kp_y, Kp_z, Kd_x, Kd_y, Kd_z = 0.;
-		double Kr = 8.0;
+		enable_output_ = false;
 
+		// get vehicle parameters
+		double mass;
+	  if (!nh_private_.param<double>("riseq/mass", mass, 1.0)){
+	      std::cout << "Did not get mass from the params, defaulting to 1.0" << std::endl;
+	  }			
 
-		/*
-	  if (!ros::param::get("riseq/Kp_x", Kp_x, 8.0)){
-	      std::cout << "Did not get bool Kp_x from the params, defaulting to 8.0" << std::endl;
+  	Eigen::Matrix3d vehicleInertia;
+	  if (!nh_private_.param<double>("riseq/Ixx", vehicleInertia(0, 0), 0.1)){
+	      std::cout << "Did not get Kp_x from the params, defaulting to 8.0" << std::endl;
 	  }		
-	  if (!ros::param::get("riseq/Kp_x", Kp_x, 8.0)){
-	      std::cout << "Did not get bool Kp_x from the params, defaulting to 8.0" << std::endl;
+	  if (!nh_private_.param<double>("riseq/Iyy", vehicleInertia(1, 1), 0.1)){
+	      std::cout << "Did not get Kp_y from the params, defaulting to 8.0" << std::endl;
 	  }	
-	  if (!ros::param::get("riseq/Kp_x", Kp_x, 8.0)){
-	      std::cout << "Did not get bool Kp_x from the params, defaulting to 8.0" << std::endl;
+	  if (!nh_private_.param<double>("riseq/Izz", vehicleInertia(2, 2), 0.1)){
+	      std::cout << "Did not get Kp_z from the params, defaulting to 8.0" << std::endl;
+	  }
+
+		double Kp_x, Kp_y, Kp_z, Kd_x, Kd_y, Kd_z, Kr = 0.;
+
+	  if (!nh_private_.param<double>("riseq/Kp_x", Kp_x, 4.0)){
+	      std::cout << "Did not get Kp_x from the params, defaulting to 8.0" << std::endl;
+	  }		
+	  if (!nh_private_.param<double>("riseq/Kp_y", Kp_y, 4.0)){
+	      std::cout << "Did not get Kp_y from the params, defaulting to 8.0" << std::endl;
 	  }	
-	  if (!ros::param::get("riseq/Kp_x", Kp_x, 8.0)){
-	      std::cout << "Did not get bool Kp_x from the params, defaulting to 8.0" << std::endl;
+	  if (!nh_private_.param<double>("riseq/Kp_z", Kp_z, 4.5)){
+	      std::cout << "Did not get Kp_z from the params, defaulting to 8.0" << std::endl;
 	  }	
-	  if (!ros::param::get("riseq/Kp_x", Kp_x, 8.0)){
-	      std::cout << "Did not get bool Kp_x from the params, defaulting to 8.0" << std::endl;
+	  if (!nh_private_.param<double>("riseq/Kd_x", Kd_x, 1.0)){
+	      std::cout << "Did not get Kd_x from the params, defaulting to 8.0" << std::endl;
 	  }	
-		*/
+	  if (!nh_private_.param<double>("riseq/Kd_y", Kd_y, 1.0)){
+	      std::cout << "Did not get Kd_y from the params, defaulting to 8.0" << std::endl;
+	  }	
+	  if (!nh_private_.param<double>("riseq/Kd_z", Kd_z, 1.0)){
+	      std::cout << "Did not get Kd_z from the params, defaulting to 8.0" << std::endl;
+	  }
+	  if (!nh_private_.param<double>("riseq/Kr", Kr, 8.0)){
+	      std::cout << "Did not get Kr from the params, defaulting to 8.0" << std::endl;
+	  }
 
-		Eigen::Vector3d Kp(Kp_x, Kp_y, Kp_z);
-		Eigen::Vector3d Kd(Kd_x, Kd_y, Kd_z);
-		Eigen::Vector3d Ki = Eigen::Vector3d::Zero();
+		Kp_ << Kp_x, Kp_y, Kp_z;
+		Kd_ << Kd_x, Kd_y, Kd_z;
+		Ki_ << 0., 0., 0.;
+		Kr_  = Kr;
 
-		fb_controller_ = new FeedbackLinearizationController(1.0, 1.0, 1.0, Eigen::Matrix3d::Identity(), Kp, Kd, Ki, Kr);
-
-
-
+		fb_controller_ = new FeedbackLinearizationController(mass, 1.0, 1.0, vehicleInertia, Kp_, Kd_, Ki_, Kr_);
 	}
 
 	void PositionController::computeControlInputs(const ros::TimerEvent& event)
 	{
-		Eigen::Vector3d thrust_vector_;
-		thrust_vector_ = fb_controller_->computeDesiredThrustVector(p_, p_ref_, v_, v_ref_, a_ref_);
-		fb_controller_->computeDesiredOrientation();
-		fb_controller_->computeDesiredAngularVelocity();
-
-		publishControlInputs();
+		if(enable_output_)
+		{
+			Eigen::Vector3d a_des;
+			a_des = fb_controller_->computeDesiredAcceleration(p_, p_ref_, v_, v_ref_, a_ref_);
+			thrust_vector_ = fb_controller_->computeDesiredThrustVector(q_, a_des);
+			desired_orientation_ = fb_controller_->computeDesiredOrientation(thrust_vector_, yaw_ref_);
+			//desired_angular_velocity_ = fb_controller_->computeDesiredAngularVelocity(q_.normalized().toRotationMatrix(), desired_orientation_, euler_dot_ref_);
+		}
+		else
+		{
+			// do nothing if reference trajectory is not received
+		}
 	}
+
+	void PositionController::computeMidControlInputs(const ros::TimerEvent& event)
+	{	
+		if(enable_output_)
+		{
+			desired_angular_velocity_ = fb_controller_->computeDesiredAngularVelocity(q_.toRotationMatrix(), desired_orientation_, euler_dot_ref_);
+			publishControlInputs();
+		}
+		else
+		{
+			// do nothing if reference trajectory is not received
+		}
+	}
+
 
 	void PositionController::publishControlInputs(void)
 	{
@@ -91,10 +128,23 @@ PositionController::PositionController(const ros::NodeHandle& nh, const ros::Nod
 
 		input_publisher_.publish(command_msg);
 
+		//debugging only
+
+		Eigen::Quaterniond des_q(desired_orientation_);
+
+		geometry_msgs::PoseStamped desired_orientation_msg;
+		desired_orientation_msg.header.stamp = ros::Time::now();
+		desired_orientation_msg.header.frame_id = "map";
+		desired_orientation_msg.pose.orientation.x = des_q.x();
+		desired_orientation_msg.pose.orientation.y = des_q.y();
+		desired_orientation_msg.pose.orientation.z = des_q.z();
+		desired_orientation_msg.pose.orientation.w = des_q.w();
+		rdes_publisher_.publish(desired_orientation_msg);
 	}
 
 	void PositionController::trajectoryCallback(const riseq_msgs::FlatTrajectory& msg)
 	{
+		enable_output_ = true;
 		p_ref_ << msg.pose.position.x, msg.pose.position.y, msg.pose.position.z;
 		v_ref_ << msg.twist.linear.x, msg.twist.linear.y, msg.twist.linear.z;
 		a_ref_ << msg.acc.x, msg.acc.y, msg.acc.z;
@@ -108,6 +158,7 @@ PositionController::PositionController(const ros::NodeHandle& nh, const ros::Nod
 		yaw_ddot_ref_ = msg.yawddot;
 
 		angular_velocity_ref_ << msg.twist.angular.x, msg.twist.angular.y, msg.twist.angular.z;
+		euler_dot_ref_ << msg.uc.x, msg.uc.y, msg.uc.z;
 	}
 
 	void PositionController::odometryCallback(const nav_msgs::Odometry& msg)
@@ -116,6 +167,7 @@ PositionController::PositionController(const ros::NodeHandle& nh, const ros::Nod
 		v_ << msg.twist.twist.linear.x, msg.twist.twist.linear.y, msg.twist.twist.linear.z;
 		q_.w() =  msg.pose.pose.orientation.w;
 		q_.vec() << msg.pose.pose.orientation.x, msg.pose.pose.orientation.y, msg.pose.pose.orientation.z;
+		q_ = q_.conjugate(); //transform from world-to-body to body-to-world rotation
 		angular_velocity_ << msg.twist.twist.angular.x , msg.twist.twist.angular.y, msg.twist.twist.angular.z;
 	}
 
@@ -130,6 +182,38 @@ PositionController::PositionController(const ros::NodeHandle& nh, const ros::Nod
 	{
 		v_ << msg.twist.linear.x, msg.twist.linear.y, msg.twist.linear.z;
 		angular_velocity_ << msg.twist.angular.x , msg.twist.angular.y, msg.twist.angular.z;
+	}
+
+	void PositionController::initializeParameters(void)
+	{
+		double Kp_x, Kp_y, Kp_z, Kd_x, Kd_y, Kd_z, Kr = 0.;
+
+	  if (!nh_private_.param<double>("riseq/Kp_x", Kp_x, 8.0)){
+	      std::cout << "Did not get bool Kp_x from the params, defaulting to 8.0" << std::endl;
+	  }		
+	  if (!nh_private_.param<double>("riseq/Kp_y", Kp_y, 8.0)){
+	      std::cout << "Did not get bool Kp_y from the params, defaulting to 8.0" << std::endl;
+	  }	
+	  if (!nh_private_.param<double>("riseq/Kp_z", Kp_z, 8.0)){
+	      std::cout << "Did not get bool Kp_z from the params, defaulting to 8.0" << std::endl;
+	  }	
+	  if (!nh_private_.param<double>("riseq/Kd_x", Kd_x, 8.0)){
+	      std::cout << "Did not get bool Kd_x from the params, defaulting to 8.0" << std::endl;
+	  }	
+	  if (!nh_private_.param<double>("riseq/Kd_y", Kd_y, 8.0)){
+	      std::cout << "Did not get bool Kd_y from the params, defaulting to 8.0" << std::endl;
+	  }	
+	  if (!nh_private_.param<double>("riseq/Kd_z", Kd_z, 8.0)){
+	      std::cout << "Did not get bool Kd_z from the params, defaulting to 8.0" << std::endl;
+	  }
+	  if (!nh_private_.param<double>("riseq/Kr", Kr, 8.0)){
+	      std::cout << "Did not get bool Kr from the params, defaulting to 8.0" << std::endl;
+	  }
+
+		Kp_ << Kp_x, Kp_y, Kp_z;
+		Kd_ << Kd_x, Kd_y, Kd_z;
+		Ki_ << 0., 0., 0.;
+		Kr_  = Kr;
 	}
 
 	PositionController::~PositionController()
